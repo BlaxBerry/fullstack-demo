ff4101ffb4a9074080f11072eb3d37ca5280dadc
BlaxBerry <chenjiaxu333@gmail.com>
2021-11-01 22:55:03 +0900

    init

--- admin/src/api/category/create.js
+++
@@ -1,9 +0,0 @@
-import instance from "../request";
-const create = (data) => {
-    instance({
-        method: 'POST',
-        url: '/category/create',
-        data
-    })
-}
-export default create
--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home/index.vue'
+import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -8,24 +8,16 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home,
-    children: [
-      {
-        path: '/category/create',
-        component: () => import('../views/Category/Create/index.vue')
-      },
-      {
-        path: 'category/list',
-        component: () => import('../views/Category/List/index.vue')
-      },
-    ]
+    component: Home
   },
-
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
 ]
 
 const router = new VueRouter({
