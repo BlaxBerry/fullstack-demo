f601a95bb16f18d8eaa9fea35f13cbcb80acb734
陳嘉旭 <73395592+BlaxBerry@users.noreply.github.com>
2022-05-21 18:55:10 +0900

    fix: change Layout components import

---
+++ adminfront/src/components/Template/RightContent/Header.tsx
@@ -0,0 +1,28 @@
+import * as React from "react"
+import { Dropdown } from "antd"
+import { Layout as AntdLayout, LayoutProps } from "antd"
+const { Header: AntdHeader } = AntdLayout
+
+import { SettingOutlined as AntdIconSetting } from "@ant-design/icons/lib/icons"
+import { HeaderSetting } from "../../Setting"
+
+const ContentHeader = (): JSX.Element => {
+  return (
+    <AntdHeader
+      style={{ textAlign: "right", color: "white", padding: "0 20px" }}
+    >
+      {/* <div style={{ textAlign: "right", color: "white", padding: "0 20px" }}> */}
+
+      {/* setting 设置下拉菜单 */}
+      <Dropdown overlay={<HeaderSetting />} placement="bottomRight">
+        <AntdIconSetting />
+      </Dropdown>
+
+      {/* TDOD: 按需添加 */}
+
+      {/* </div> */}
+    </AntdHeader>
+  )
+}
+
+export default ContentHeader
--- adminfront/src/components/Template/index.tsx
+++ adminfront/src/components/Template/index.tsx
@@ -1,37 +1,24 @@
 import * as React from "react"
 import { Layout as AntdLayout } from "antd"
-import LeftSider from "./Sider/LeftSider"
-import TopHeader from "./Header"
-import ContentHeader from "./Header/ContentHeader"
 import { PageTitle } from "../Common/Titles"
+import RightContent from "./RightContent"
+import LeftSider from "./LeftSideBar"
 
-const { Content: AntdContent, Footer: AntdFooter } = AntdLayout
-
-interface LayoutProps {
+export interface LayoutProps {
   pageTitle: string
   children?: React.ReactNode
 }
 
 const Layout = (props: LayoutProps): JSX.Element => (
   <AntdLayout>
+    {/* 页面标签相关 */}
     <PageTitle title={props.pageTitle} />
-    {/* left sider */}
-    <LeftSider style={{ height: "100vh" }} />
 
-    {/* right content */}
-    <AntdLayout>
-      {/* header */}
-      <TopHeader>
-        <ContentHeader />
-      </TopHeader>
-      {/* content */}
-      <AntdContent className="theme-light">
-        {/* true content */}
-        <div className="true-content">{props.children}</div>
-        {/* footer */}
-        <AntdFooter style={{ textAlign: "center" }}>@BlaxBerry</AntdFooter>
-      </AntdContent>
-    </AntdLayout>
+    {/* 布局左侧*/}
+    <LeftSider />
+
+    {/* 页面内容*/}
+    <RightContent>{props.children}</RightContent>
   </AntdLayout>
 )
 
