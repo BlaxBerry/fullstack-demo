8ed21f1f073ae87d2171a3d25be127fd5e35e4f8
陳嘉旭 <73395592+BlaxBerry@users.noreply.github.com>
2022-05-22 22:17:38 +0900

    fix: update eslint config

--- adminfront/.eslintrc.js
+++ adminfront/.eslintrc.js
@@ -1,22 +1,25 @@
 module.exports = {
-  globals: {
-    __PATH_PREFIX__: true,
-  },
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
   extends: [
-    "plugin:@typescript-eslint/recommended",
     "react-app",
-    // "prettier",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:storybook/recommended",
     // "plugin:react-hooks/recommended",
     // "plugin:storybook/recommended",
+    // "plugin:react/recommended",
+    // "plugin:prettier/recommended",
+    // "prettier/@typescript-eslint",
+    // "prettier",
   ],
   env: {
     browser: true,
     node: true,
+    es6: true,
   },
   rules: {
     "@typescript-eslint/no-empty-interface": 0,
+    // allow @ts-ignore for testing purposes
     "@typescript-eslint/ban-ts-comment": "off",
   },
+  plugins: ["@typescript-eslint"],
 }
